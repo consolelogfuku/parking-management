4a18a9709888a64a29baf9775c76acd9d83d4fe7
fukumasa sugawa <fuku1223sugawa@gmail.com>
2026-04-13 15:57:37 +0900

    静的ファイルをcloudfrontで配信するように修正

--- infra/acm.tf
+++ infra/acm.tf
@@ -17,3 +17,26 @@ resource "aws_acm_certificate_validation" "main" {
   certificate_arn = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
+
+# 静的コンテンツ用ACM証明書を作成する
+resource "aws_acm_certificate" "assets" {
+  provider          = aws.us_east_1 # CloudFrontはus-east-1で動作するため、us-east-1プロバイダを使用する
+  domain_name       = "assets.parking-checker.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "parking-management-assets-cert"
+  }
+}
+
+# 静的コンテンツ用ACM証明書を検証する
+resource "aws_acm_certificate_validation" "assets" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.assets.arn
+  validation_record_fqdns = [for record in aws_route53_record.assets_cert_validation :
+record.fqdn]
+}
