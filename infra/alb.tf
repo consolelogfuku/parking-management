d7aa1c23554f2d7cab0fd4fd345e9700993dba52
fukumasa sugawa <fuku1223sugawa@gmail.com>
2026-04-14 12:42:51 +0900

    通常のリクエストもcloudFront通るように & WAFの設定

--- infra/alb.tf
+++ infra/alb.tf
@@ -7,21 +7,29 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-resource "aws_security_group_rule" "alb_sg_rule_inbound_http" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.alb_sg.id
+# CloudFrontのIPアドレス範囲を取得(プレフィックスリスト)
+data "aws_ec2_managed_prefix_list" "cloudfront_prefix_list" {
+  name = "com.amazonaws.global.cloudfront.origin-facing"
 }
+
+# CloudFrontが、httpでアクセスしてきた場合は、httpsにリダイレクトするため、httpは不要
+# resource "aws_security_group_rule" "alb_sg_rule_inbound_http" {
+#   type = "ingress"
+#   from_port = 80
+#   to_port = 80
+#   protocol = "tcp"
+#   # cidr_blocks = ["0.0.0.0/0"] # Route53=>ALBの場合は、これを使う。
+#   prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront_prefix_list.id] # CloudFront=>ALBの場合は、これを使う。
+#   security_group_id = aws_security_group.alb_sg.id
+# }
 
 resource "aws_security_group_rule" "alb_sg_rule_inbound_https" {
   type = "ingress"
   from_port = 443
   to_port = 443
   protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  # cidr_blocks = ["0.0.0.0/0"] # Route53=>ALBの場合は、これを使う。
+  prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront_prefix_list.id] # CloudFront=>ALBの場合は、これを使う。
   security_group_id = aws_security_group.alb_sg.id
 }
 
@@ -73,19 +81,20 @@ resource "aws_lb_target_group_attachment" "app" {
   port = 80 # ALBが、EC2の80ポートに流す(Nginx)
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_alb.main.arn
-  port = 80
-  protocol = "HTTP"
-  default_action {
-    type = "redirect" # HTTPからHTTPSへリダイレクトする
-    redirect {
-      port = "443"
-      protocol = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
+# HTTPSでしか受け付けないためHTTPリスナーは不要(cloudfrontがhttp=>httpsへリダイレクトするため)
+# resource "aws_lb_listener" "http" {
+#   load_balancer_arn = aws_alb.main.arn
+#   port = 80
+#   protocol = "HTTP"
+#   default_action {
+#     type = "redirect" # HTTPからHTTPSへリダイレクトする
+#     redirect {
+#       port = "443"
+#       protocol = "HTTPS"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
 
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_alb.main.arn
---
+++ infra/waf.tf
@@ -0,0 +1,86 @@
+# WAF Web ACL（CloudFrontに適用）
+resource "aws_wafv2_web_acl" "main" {
+  provider    = aws.us_east_1
+  name        = "parking-checker-waf"
+  description = "WAF for parking-checker CloudFront"
+  scope       = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  # AWS管理ルール: 一般的な脅威からの保護(Core rule set)
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # AWS管理ルール: SQLインジェクション対策(SQL database)
+  rule {
+    name     = "AWSManagedRulesSQLiRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesSQLiRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # AWS管理ルール: Amazon IP評価リスト(Amazon IP reputation list)
+  rule {
+    name     = "AWSManagedRulesAmazonIpReputationList"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAmazonIpReputationList"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "parking-checker-waf"
+    sampled_requests_enabled   = true
+  }
+}
